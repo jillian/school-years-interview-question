a6514a943ac4d0f5e7a995729b84b8d0c85a1d87
Jillian Macnaughton <jillian@raise.me>
2019-07-10 11:37:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 // import {} from 'lodash';
 
-// takes an array of school year ranges, as strings, and returns a list of numbers
-// ex. input of ["2014-2015", "2015-2016", "2016-2017", "2019-2020"] would produce: [2014, 2015, 2016, 2017, 2019, 2020]
+// takes an array of school year ranges, as strings, 
+// and returns a list of numbers
+// ex. input of ["2014-2015", "2015-2016", "2016-2017", "2019-2020"] 
+// would produce: [2014, 2015, 2016, 2017, 2019, 2020]
 export const parseSchoolYears = (schoolYearsRanges) => {
   // Implementation
 };
 
 
-// takes an array of years (does not need to be sorted) and produces an array of strings of the year ranges
-// ex. input of [2014, 2015, 2016, 2017, 2019, 2020] would produce: ["2014 - 2017", "2019 - 2020"]
+// takes an array of years (does not need to be sorted) and produces 
+// an array of strings of the year ranges
+// ex. input of [2014, 2015, 2016, 2017, 2019, 2020] 
+// would produce: ["2014 - 2017", "2019 - 2020"]
 export const yearRanges = (years) => {
   // Implementation
 };
 
 
-// takes an array of school year strings (such as 2014-2015) and consolidates the ranges
-// input does not need to be sorted.
-// ex. input of ["2014-2015", "2015-2016", "2016-2017", "2019-2020"] would produce: ["2014 - 2017", "2019 - 2020"]
+// takes an array of school year strings (such as 2014-2015) 
+// and consolidates the ranges input does not need to be sorted.
+// ex. input of ["2014-2015", "2015-2016", "2016-2017", "2019-2020"] 
+// would produce: ["2014 - 2017", "2019 - 2020"]
 export const consolidateSchoolYears = (schoolYearRanges) => {
   return yearRanges(parseSchoolYears(schoolYearRanges));
 };
